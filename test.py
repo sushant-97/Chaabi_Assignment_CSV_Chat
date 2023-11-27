841a44c0584fcbf3d9a09fa606f99af25d9eaad3
sushant-97 <65329646+sushant-97@users.noreply.github.com>
2023-11-27 11:57:22 +0530

    Update test.py
    upadted command line arguments

--- test.py
+++ test.py
@@ -16,20 +16,17 @@ def make_post_request(url, form_data):
     except requests.RequestException as e:
         print(f"Error making the request: {e}")
 
-# # Replace 'your_api_endpoint' with the actual API endpoint URL
-# api_url = 'http://192.168.251.119:8888/query'
-
 def main():
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description='Make a POST request to an API')
-    parser.add_argument('api_url', type=str, required=False, help='API endpoint URL')
-    parser.add_argument('user_prompt', type=str, required=True, help='User prompt for the API request')
+    parser.add_argument('--api_url', type=str, required=False, help='API endpoint URL')
+    parser.add_argument('--user_prompt', type=str, required=True, help='User prompt for the API request')
     
 
     args = parser.parse_args()
 
     # Replace 'your_api_endpoint' with the actual API endpoint URL
-    # api_url = 'http://192.168.251.119:8888/query'
+    # api_url = 'http://192.168.2.119:8888/query'
     api_url_ = args.api_url
 
     # Use the user-provided prompt as form data
@@ -42,3 +39,32 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+# import requests
+
+# def make_post_request(url, form_data):
+#     try:
+#         response = requests.post(url, data=form_data)
+        
+#         # Check if the request was successful (status code 200)
+#         if response.status_code == 200:
+#             print("Request was successful!")
+#             print("Response data:", response.text)
+#         else:
+#             print(f"Request failed with status code {response.status_code}")
+#             print("Response data:", response.text)
+            
+#     except requests.RequestException as e:
+#         print(f"Error making the request: {e}")
+
+# # Replace 'your_api_endpoint' with the actual API endpoint URL
+# api_url = 'http://192.168.2.119:8888/query'
+
+# # Replace 'your_key' and 'your_value' with the actual form data parameters
+# form_data = {
+#     'user_prompt': 'what is the rate of nissin noodles?'
+# }
+
+# make_post_request(api_url, form_data)
